fc4c78d9c295c3aaca3840ea5575609fec9c51ad
tagore711 <jagatatagore711@gmail.com>
2025-10-11 23:00:23 +0530

    Enable SSL for PostgreSQL connection in production

--- db.js
+++ db.js
@@ -9,6 +9,7 @@ const pool = new Pool({
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 // Test connection when the pool is created
